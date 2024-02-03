01f655bfd50dc9a055a73f04ba18d218bca59bcf
voidfoxtamcin <voidfoxtamcin@gmail.com>
2024-02-03 21:27:35 +0700

    first commit

---
+++ src/components/Navbar.svelte
@@ -0,0 +1,27 @@
+<nav class="bg-white border-b">
+  <div class="container mx-auto flex justify-between items-center py-4">
+    <a href="/" class="inline-block font-bold text-xl text-gray-600">Tamcin</a>
+    <div class="flex gap-x-6">
+      <a
+        href="/"
+        class="inline-block font-semibold text-gray-600 hover:text-blue-600"
+        >Home</a
+      >
+      <a
+        href="/"
+        class="inline-block font-semibold text-gray-600 hover:text-blue-600"
+        >Gallery</a
+      >
+      <a
+        href="/"
+        class="inline-block font-semibold text-gray-600 hover:text-blue-600"
+        >Blog</a
+      >
+      <a
+        href="/"
+        class="inline-block font-semibold text-gray-600 hover:text-blue-600"
+        >About</a
+      >
+    </div>
+  </div>
+</nav>
