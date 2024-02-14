bff508d24649cd880bb25dbd41fc43ca93f32d1a
voidfoxtamcin <voidfoxtamcin@gmail.com>
2024-02-14 14:15:36 +0700

    configuration update

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,12 +2,13 @@ import { defineConfig } from 'astro/config';
 import tailwind from "@astrojs/tailwind";
 import svelte from "@astrojs/svelte";
 import mdx from "@astrojs/mdx";
-
 import vercel from "@astrojs/vercel/serverless";
+
+import icon from "astro-icon";
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [tailwind(), svelte(), mdx()],
+  integrations: [tailwind(), svelte(), mdx(), icon()],
   output: "server",
   adapter: vercel()
 });
