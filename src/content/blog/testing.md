b471fc128563ba076ac9daba65c80dfc7b092735
voidfoxtamcin <voidfoxtamcin@gmail.com>
2024-02-14 16:08:59 +0700

    added blog and my orignal characters section

---
+++ src/content/blog/testing.md
@@ -0,0 +1,12 @@
+---
+title: Testing
+tags:
+    - personal
+date: 2024-02-13
+published: true
+---
+
+# Testing
+This is a test if the blog is working or not.
+
+Looks like it's working lol.
---
+++ src/content/config.ts
@@ -0,0 +1,28 @@
+import { z, defineCollection } from 'astro:content';
+
+const blogCollection = defineCollection({
+    type: 'content',
+    schema: z.object({
+        title: z.string(),
+        tags: z.array(z.string()),
+        date: z.date(),
+        image: z.string().optional(),
+        published: z.boolean().default(false),
+    })
+})
+
+const ocCollection = defineCollection({
+    type: 'data',
+    schema: z.object({
+        name: z.string(),
+        gender: z.string(),
+        species: z.string(),
+        image: z.string(),
+        description: z.string()
+    })
+})
+
+export const collections = {
+    blog: blogCollection,
+    oc: ocCollection
+}
